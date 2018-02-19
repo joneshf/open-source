849dfab088c600b41f8205b81201cced12df4623
joneshf <jones3.hardy@gmail.com>
2018-02-19 01:41:31 -0800

    Accept more configuration to the scribe
    We probably should lump all this stuff into one data type.
    But this should be good enough for a first release.

--- src/Katip/Scribes/Rollbar.hs
+++ src/Katip/Scribes/Rollbar.hs
@@ -1,6 +1,7 @@
 {-# LANGUAGE DataKinds           #-}
 {-# LANGUAGE LambdaCase          #-}
 {-# LANGUAGE NamedFieldPuns      #-}
+{-# LANGUAGE OverloadedStrings   #-}
 {-# LANGUAGE PackageImports      #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE TypeOperators       #-}
@@ -26,6 +27,7 @@ import "stm-chans" Control.Concurrent.STM.TBMQueue
 import "aeson" Data.Aeson                          (Value)
 import "text" Data.Text.Lazy                       (toStrict)
 import "text" Data.Text.Lazy.Builder               (toLazyText)
+import "time" Data.Time.Clock                      (UTCTime)
 import "katip" Katip
     ( LogItem
     , Scribe(Scribe, liPush, scribeFinalizer)
@@ -36,9 +38,12 @@ import "katip" Katip
     , permitItem
     , unLogStr
     , _itemEnv
+    , _itemHost
     , _itemMessage
     , _itemSeverity
+    , _itemTime
     )
+import "hostname" Network.HostName                 (HostName)
 import "http-client" Network.HTTP.Client           (Manager)
 import "rollbar-hs" Rollbar.AccessToken            (AccessToken)
 import "rollbar-hs" Rollbar.API                    (itemsPOST')
@@ -51,9 +56,16 @@ import "rollbar-hs" Rollbar.Item
     , warning
     )
 import "rollbar-hs" Rollbar.Item.Body              (MessageBody(MessageBody))
-import "rollbar-hs" Rollbar.Item.Data              (Data)
+import "rollbar-hs" Rollbar.Item.CodeVersion       (CodeVersion)
+import "rollbar-hs" Rollbar.Item.Data
+    ( Data(framework, server, timestamp)
+    )
 import "rollbar-hs" Rollbar.Item.Environment       (Environment(Environment))
 import "rollbar-hs" Rollbar.Item.MissingHeaders    (RemoveHeaders)
+import "rollbar-hs" Rollbar.Item.Server
+    ( Branch
+    , Server(Server, branch, host, root, serverCodeVersion)
+    )
 
 import qualified "katip" Katip
 
@@ -67,17 +79,19 @@ mkRollbarScribe ::
   RemoveHeaders headers =>
   proxy headers ->
   AccessToken ->
+  Maybe Branch ->
+  Maybe CodeVersion ->
   Manager ->
   Severity ->
   Verbosity ->
   IO Scribe
-mkRollbarScribe proxy accessToken manager severity verbosity = do
+mkRollbarScribe proxy accessToken branch codeVersion manager severity verbosity = do
   queue <- newTBMQueueIO queueSize
   finalize <- newEmptyMVar
   setupWorkers proxy queue finalize manager
   let liPush item = when (permitItem severity item) $
         atomically (writeTBMQueue queue $ rollbarItem' item)
-      rollbarItem' item = rollbarItem proxy accessToken verbosity item
+      rollbarItem' item = rollbarItem proxy accessToken branch codeVersion verbosity item
       scribeFinalizer = do
         putMVar finalize ()
         takeMVar finalize
@@ -87,15 +101,26 @@ rollbarItem ::
   (LogItem a, RemoveHeaders headers) =>
   proxy headers ->
   AccessToken ->
+  Maybe Branch ->
+  Maybe CodeVersion ->
   Verbosity ->
   Katip.Item a ->
   Item Value ("Authorization" ': headers)
-rollbarItem _ accessToken verbosity item = Item { accessToken, itemData }
+rollbarItem _ accessToken branch serverCodeVersion verbosity item =
+  Item { accessToken, itemData }
   where
   environment :: Environment
   environment = Environment (getEnvironment $ _itemEnv item)
+  hostName :: HostName
+  hostName = _itemHost item
   itemData :: Data Value ("Authorization" ': headers)
-  itemData = case severity of
+  itemData = itemData'
+    { framework = Just "katip"
+    , server = Just server
+    , timestamp = Just timestamp
+    }
+  itemData' :: Data Value ("Authorization" ': headers)
+  itemData' = case severity of
     DebugS   -> debug environment (Just messageBody) value
     InfoS    -> info environment (Just messageBody) value
     NoticeS  -> info environment (Just messageBody) value
@@ -105,8 +130,12 @@ rollbarItem _ accessToken verbosity item = Item { accessToken, itemData }
   messageBody :: MessageBody
   messageBody =
     MessageBody (toStrict $ toLazyText $ unLogStr $ _itemMessage item)
+  server :: Server
+  server = Server { branch, host = Just hostName, root = Nothing, serverCodeVersion }
   severity :: Severity
   severity = _itemSeverity item
+  timestamp :: UTCTime
+  timestamp = _itemTime item
   value :: Value
   value = itemJson verbosity item
 
