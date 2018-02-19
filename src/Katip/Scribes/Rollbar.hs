f1efcf524b9ded6e9f56b10406cbfeb7dd03493d
joneshf <jones3.hardy@gmail.com>
2018-02-19 00:37:03 -0800

    Set up a queue to finalize the scribe
    This implementation is inspired by `katip-elasitcsearch`s finalizer.
    
    We want to gracefully handle a connection mid-flight.
    If we keep track of everything in a queue,
    we can shut it down when we're ready to finalize,
    but still keep track of the in-flight requests.
    
    There are probably bugs in here...

--- src/Katip/Scribes/Rollbar.hs
+++ src/Katip/Scribes/Rollbar.hs
@@ -1,21 +1,31 @@
+{-# LANGUAGE DataKinds           #-}
+{-# LANGUAGE LambdaCase          #-}
+{-# LANGUAGE NamedFieldPuns      #-}
+{-# LANGUAGE PackageImports      #-}
 {-# LANGUAGE ScopedTypeVariables #-}
-{-# LANGUAGE DataKinds      #-}
-{-# LANGUAGE LambdaCase     #-}
-{-# LANGUAGE NamedFieldPuns #-}
-{-# LANGUAGE PackageImports #-}
-{-# LANGUAGE TypeOperators  #-}
+{-# LANGUAGE TypeOperators       #-}
 
 module Katip.Scribes.Rollbar where
 
 import Prelude hiding (error)
 
-import "base" Control.Exception (SomeException, Exception, catch)
-import "base" Control.Monad (when)
-import "base" Data.Functor  (void)
+import "base" Control.Concurrent.MVar (newEmptyMVar, putMVar, takeMVar)
+import "base" Control.Monad           (replicateM, when)
+import "base" Data.Foldable           (for_)
+import "base" Data.Functor            (void)
+import "base" GHC.Conc                (atomically)
 
-import "aeson" Data.Aeson                       (Value)
-import "text" Data.Text.Lazy                    (toStrict)
-import "text" Data.Text.Lazy.Builder            (toLazyText)
+import "async" Control.Concurrent.Async            (async, waitCatchSTM)
+import "stm-chans" Control.Concurrent.STM.TBMQueue
+    ( TBMQueue
+    , closeTBMQueue
+    , newTBMQueueIO
+    , readTBMQueue
+    , writeTBMQueue
+    )
+import "aeson" Data.Aeson                          (Value)
+import "text" Data.Text.Lazy                       (toStrict)
+import "text" Data.Text.Lazy.Builder               (toLazyText)
 import "katip" Katip
     ( LogItem
     , Scribe(Scribe, liPush, scribeFinalizer)
@@ -23,16 +33,15 @@ import "katip" Katip
     , Verbosity
     , getEnvironment
     , itemJson
-    , payloadObject
     , permitItem
     , unLogStr
     , _itemEnv
     , _itemMessage
     , _itemSeverity
     )
-import "http-client" Network.HTTP.Client        (Manager)
-import "rollbar-hs" Rollbar.AccessToken         (AccessToken)
-import "rollbar-hs" Rollbar.API                 (itemsPOST')
+import "http-client" Network.HTTP.Client           (Manager)
+import "rollbar-hs" Rollbar.AccessToken            (AccessToken)
+import "rollbar-hs" Rollbar.API                    (itemsPOST')
 import "rollbar-hs" Rollbar.Item
     ( Item(Item, accessToken, itemData)
     , critical
@@ -41,12 +50,18 @@ import "rollbar-hs" Rollbar.Item
     , info
     , warning
     )
-import "rollbar-hs" Rollbar.Item.Body           (MessageBody(MessageBody))
-import "rollbar-hs" Rollbar.Item.Data           (Data)
-import "rollbar-hs" Rollbar.Item.Environment    (Environment(Environment))
-import "rollbar-hs" Rollbar.Item.MissingHeaders (RemoveHeaders)
+import "rollbar-hs" Rollbar.Item.Body              (MessageBody(MessageBody))
+import "rollbar-hs" Rollbar.Item.Data              (Data)
+import "rollbar-hs" Rollbar.Item.Environment       (Environment(Environment))
+import "rollbar-hs" Rollbar.Item.MissingHeaders    (RemoveHeaders)
 
 import qualified "katip" Katip
+
+queueSize :: Int
+queueSize = 10
+
+workerSize :: Int
+workerSize = 3
 
 mkRollbarScribe ::
   RemoveHeaders headers =>
@@ -57,10 +72,23 @@ mkRollbarScribe ::
   Verbosity ->
   IO Scribe
 mkRollbarScribe proxy accessToken manager severity verbosity = do
-  let liPush item = when (permitItem severity item) $ do
-        void (itemsPOST' manager $ rollbarItem' item)
+  queue <- newTBMQueueIO queueSize
+  finalize <- newEmptyMVar
+  void $ async $ do
+    -- Setup the workers and wait until the finalizer is evaluated.
+    -- Then, close the queueu, wait for the workers and continue finalization.
+    workers <- replicateM workerSize (async $ mkWorker proxy manager queue)
+    takeMVar finalize
+    atomically $ do
+      closeTBMQueue queue
+      for_ workers waitCatchSTM
+    putMVar finalize ()
+  let liPush item = when (permitItem severity item) $
+        atomically (writeTBMQueue queue $ rollbarItem' item)
       rollbarItem' item = rollbarItem proxy accessToken item verbosity
-      scribeFinalizer = pure ()
+      scribeFinalizer = do
+        putMVar finalize ()
+        takeMVar finalize
   pure Scribe { liPush, scribeFinalizer }
 
 rollbarItem ::
@@ -89,3 +117,17 @@ rollbarItem _ accessToken item verbosity = Item { accessToken, itemData }
   severity = _itemSeverity item
   value :: Value
   value = itemJson verbosity item
+
+mkWorker ::
+  (RemoveHeaders headers) =>
+  proxy headers ->
+  Manager ->
+  TBMQueue (Item Value ("Authorization" ': headers)) ->
+  IO ()
+mkWorker _ manager queue = go
+  where
+  go = do
+    item' <- atomically (readTBMQueue queue)
+    for_ item' $ \item -> do
+      void (itemsPOST' manager item)
+      go
