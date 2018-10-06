20a62bd648720b5ad62a5cad919a7c72c7c74bcb
joneshf <jones3.hardy@gmail.com>
2018-10-06 11:53:47 -0700

    Build the rules based on configuration
    Finally, we get to the real benefit of using shake.
    Very few of the rules are known up front.
    Instead, we generate them based on the configuration of them.
    The configuration is still hard coded in the file.
    But, we can move that out separately.

--- Shakefile.hs
+++ Shakefile.hs
@@ -1,10 +1,15 @@
+{-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE NamedFieldPuns #-}
 {-# LANGUAGE PackageImports #-}
 module Main where
 
+import "base" Control.Monad.IO.Class        (liftIO)
+import "base" Data.Foldable                 (for_, toList)
+import "base" Data.List.NonEmpty            (NonEmpty((:|)))
 import "shake" Development.Shake
     ( CmdOption(Cwd, EchoStdout, FileStdout, Traced)
     , Exit(Exit)
+    , Rules
     , ShakeOptions(shakeFiles, shakeThreads, shakeVersion)
     , Stdout(Stdout)
     , cmd
@@ -37,9 +42,20 @@ import "typed-process" System.Process.Typed
     , shell
     )
 
+data Package
+  = Haskell
+    { manifest :: Manifest
+    , name     :: String
+    , tests    :: Maybe (NonEmpty String)
+    , version  :: String
+    }
+
+data Manifest
+  = Cabal
+  | Hpack
+
 main :: IO ()
 main = do
-  root <- getCurrentDirectory
   shakeVersion <- getHashedShakeVersion ["Shakefile.hs"]
   let options = shakeOptions { shakeFiles, shakeThreads, shakeVersion }
       shakeFiles = buildDir
@@ -47,314 +63,30 @@ main = do
   shakeArgs options $ do
     want ["build"]
 
-    phony "build"
-      ( need
-        [ buildKatipRollbar </> ".build"
-        , buildRollbarHS </> ".build"
-        , buildWaiMiddlewareRollbar </> ".build"
-        ]
-      )
+    phony "build" (need $ fmap build packages)
 
     phony "clean" (removeFilesAfter "" [buildDir])
 
-    phony "sdist"
-      ( need
-        [ buildKatipRollbar
-          </> katipRollbar
-          <> "-"
-          <> katipRollbarVersion
-          <> ".tar.gz"
-        , buildRollbarHS </> rollbarHS <> "-" <> rollbarHSVersion <> ".tar.gz"
-        , buildWaiMiddlewareRollbar
-          </> waiMiddlewareRollbar
-          <> "-"
-          <> waiMiddlewareRollbarVersion
-          <> ".tar.gz"
-        ]
-      )
+    phony "sdist" (need $ fmap sdist packages)
 
     phony "shell" (runAfter $ runProcess_ $ shell "nix-shell --pure")
 
-    phony "test" (need [buildRollbarHS </> "build/doc-test/doc-test.out"])
+    phony "test" (need $ foldMap test packages)
 
-    phony "upload-to-hackage"
-      ( need
-        [ buildKatipRollbar </> katipRollbar <> "-" <> katipRollbarVersion
-        , buildRollbarHS </> rollbarHS <> "-" <> rollbarHSVersion
-        , buildWaiMiddlewareRollbar
-          </> waiMiddlewareRollbar
-          <> "-"
-          <> waiMiddlewareRollbarVersion
-        ]
-      )
-
-    phony ("watch-" <> katipRollbar) $ do
-      need [buildKatipRollbar </> ".configure"]
-      runAfter
-        ( runProcess_
-        $ setWorkingDir packageKatipRollbar
-        $ proc
-          "ghcid"
-          [ "--command"
-          , "cabal repl lib:"
-            <> katipRollbar
-            <> " --builddir "
-            <> root </> buildKatipRollbar
-            <> " --ghc-options '"
-            <> unwords ghciFlags
-            <> "'"
-          ]
-        )
-
-    phony ("watch-" <> rollbarHS) $ do
-      need [buildRollbarHS </> ".configure"]
-      runAfter
-        ( runProcess_
-        $ setWorkingDir packageRollbarHS
-        $ proc
-          "ghcid"
-          [ "--command"
-          , "cabal repl lib:"
-            <> rollbarHS
-            <> " --builddir "
-            <> root </> buildRollbarHS
-            <> " --ghc-options '"
-            <> unwords ghciFlags
-            <> "'"
-          ]
-        )
-
-    phony ("watch-" <> waiMiddlewareRollbar) $ do
-      need [buildWaiMiddlewareRollbar </> ".configure"]
-      runAfter
-        ( runProcess_
-        $ setWorkingDir packageWaiMiddlewareRollbar
-        $ proc
-          "ghcid"
-          [ "--command"
-          , "cabal repl lib:"
-            <> waiMiddlewareRollbar
-            <> " --builddir "
-            <> root </> buildWaiMiddlewareRollbar
-            <> " --ghc-options '"
-            <> unwords ghciFlags
-            <> "'"
-          ]
-        )
+    phony "upload-to-hackage" (need $ fmap uploadToHackage packages)
 
     buildDir </> ".update" %> \out ->
       cmd (FileStdout out) (Traced "cabal update") "cabal update"
 
-    buildKatipRollbar </> ".build" %> \out -> do
-      srcs <- getDirectoryFiles "" [packageKatipRollbar </> "src//*.hs"]
-      need ((buildKatipRollbar </> ".configure") : srcs)
-      cmd
-        (Cwd packageKatipRollbar)
-        (FileStdout out)
-        (Traced "cabal build")
-        "cabal build"
-        "--builddir"
-        [root </> buildKatipRollbar]
+    for_ packages $ \case
+      Haskell manifest name tests version -> haskell manifest name tests version
 
-    buildRollbarHS </> ".build" %> \out -> do
-      srcs <- getDirectoryFiles "" [packageRollbarHS </> "src//*.hs"]
-      need ((buildRollbarHS </> ".configure") : srcs)
-      cmd
-        (Cwd packageRollbarHS)
-        (FileStdout out)
-        (Traced "cabal build")
-        "cabal build"
-        "--builddir"
-        [root </> buildRollbarHS]
-
-    buildWaiMiddlewareRollbar </> ".build" %> \out -> do
-      srcs <- getDirectoryFiles "" [packageWaiMiddlewareRollbar </> "src//*.hs"]
-      need ((buildWaiMiddlewareRollbar </> ".configure") : srcs)
-      cmd
-        (Cwd packageWaiMiddlewareRollbar)
-        (FileStdout out)
-        (Traced "cabal build")
-        "cabal build"
-        "--builddir"
-        [root </> buildWaiMiddlewareRollbar]
-
-    buildKatipRollbar </> ".check" %> \out -> do
-      need [packageKatipRollbar </> katipRollbar <.> "cabal"]
-      cmd
-        (Cwd packageKatipRollbar)
-        (EchoStdout True)
-        (FileStdout out)
-        (Traced "cabal check")
-        "cabal check"
-
-    buildRollbarHS </> ".check" %> \out -> do
-      need [packageRollbarHS </> rollbarHS <.> "cabal"]
-      cmd
-        (Cwd packageRollbarHS)
-        (EchoStdout True)
-        (FileStdout out)
-        (Traced "cabal check")
-        "cabal check"
-
-    buildWaiMiddlewareRollbar </> ".check" %> \out -> do
-      need [packageWaiMiddlewareRollbar </> waiMiddlewareRollbar <.> "cabal"]
-      cmd
-        (Cwd packageWaiMiddlewareRollbar)
-        (EchoStdout True)
-        (FileStdout out)
-        (Traced "cabal check")
-        "cabal check"
-
-    buildKatipRollbar </> ".configure" %> \out -> do
-      need
-        [ buildDir </> ".update"
-        , packageKatipRollbar </> katipRollbar <.> "cabal"
-        ]
-      cmd
-        (Cwd packageKatipRollbar)
-        (FileStdout out)
-        (Traced "cabal configure")
-        "cabal configure"
-        "--builddir"
-        [root </> buildKatipRollbar]
-
-    buildRollbarHS </> ".configure" %> \out -> do
-      need [buildDir </> ".update", packageRollbarHS </> rollbarHS <.> "cabal"]
-      cmd
-        (Cwd packageRollbarHS)
-        (FileStdout out)
-        (Traced "cabal configure")
-        "cabal configure"
-        "--builddir"
-        [root </> buildRollbarHS]
-        "--enable-tests"
-
-    buildWaiMiddlewareRollbar </> ".configure" %> \out -> do
-      need
-        [ buildDir </> ".update"
-        , packageWaiMiddlewareRollbar </> waiMiddlewareRollbar <.> "cabal"
-        ]
-      cmd
-        (Cwd packageWaiMiddlewareRollbar)
-        (FileStdout out)
-        (Traced "cabal configure")
-        "cabal configure"
-        "--builddir"
-        [root </> buildWaiMiddlewareRollbar]
-
-    buildRollbarHS </> "build/doc-test/doc-test" %> \_ -> do
-      srcs <-
-        getDirectoryFiles
-          ""
-          [packageRollbarHS </> "src//*.hs", packageRollbarHS </> "test//*.hs"]
-      need ((buildRollbarHS </> ".build") : srcs)
-      cmd_
-        (Cwd packageRollbarHS)
-        (Traced "cabal build")
-        "cabal build"
-        "test:doc-test"
-        "--builddir"
-        [root </> buildRollbarHS]
-
-    buildRollbarHS </> "build/doc-test/doc-test.out" %> \out -> do
-      need [dropExtension out]
-      cmd_
-        (Cwd packageRollbarHS)
-        (FileStdout out)
-        (Traced "rollbar doc-test")
-        [((root </>) . dropExtension) out]
-
-    buildKatipRollbar </> katipRollbar <> "-" <> katipRollbarVersion <.> "tar.gz" %> \_ -> do
-      srcs <- getDirectoryFiles "" [packageKatipRollbar </> "src//*.hs"]
-      need
-        ( (buildKatipRollbar </> ".check")
-        : (buildKatipRollbar </> ".configure")
-        : srcs
-        )
-      cmd_
-        (Cwd packageKatipRollbar)
-        (Traced "cabal sdist")
-        "cabal sdist"
-        "--builddir"
-        [root </> buildKatipRollbar]
-
-    buildRollbarHS </> rollbarHS <> "-" <> rollbarHSVersion <.> "tar.gz" %> \_ -> do
-      srcs <- getDirectoryFiles "" [packageRollbarHS </> "src//*.hs"]
-      need
-        ( (buildRollbarHS </> ".check")
-        : (buildRollbarHS </> ".configure")
-        : srcs
-        )
-      cmd_
-        (Cwd packageRollbarHS)
-        (Traced "cabal sdist")
-        "cabal sdist"
-        "--builddir"
-        [root </> buildRollbarHS]
-
-    buildWaiMiddlewareRollbar </> waiMiddlewareRollbar <> "-" <> waiMiddlewareRollbarVersion <.> "tar.gz" %> \_ -> do
-      srcs <- getDirectoryFiles "" [packageWaiMiddlewareRollbar </> "src//*.hs"]
-      need
-        ( (buildWaiMiddlewareRollbar </> ".check")
-        : (buildWaiMiddlewareRollbar </> ".configure")
-        : srcs
-        )
-      cmd_
-        (Cwd packageWaiMiddlewareRollbar)
-        (Traced "cabal sdist")
-        "cabal sdist"
-        "--builddir"
-        [root </> buildWaiMiddlewareRollbar]
-
-    buildKatipRollbar </> katipRollbar <> "-" <> katipRollbarVersion %> \out -> do
-      (Exit x, Stdout result) <-
-        cmd (Traced "cabal info") "cabal info" [takeFileName out]
-      case x of
-        ExitFailure _ -> do
-          need [buildKatipRollbar </> ".build", out <.> "tar.gz"]
-          cmd_ (Traced "cabal upload") "cabal upload" [out <.> "tar.gz"]
-          writeFile' out ""
-        ExitSuccess -> writeFile' out result
-
-    buildRollbarHS </> rollbarHS <> "-" <> rollbarHSVersion %> \out -> do
-      (Exit x, Stdout result) <-
-        cmd (Traced "cabal info") "cabal info" [takeFileName out]
-      case x of
-        ExitFailure _ -> do
-          need [buildRollbarHS </> ".build", out <.> "tar.gz"]
-          cmd_ (Traced "cabal upload") "cabal upload" [out <.> "tar.gz"]
-          writeFile' out ""
-        ExitSuccess -> writeFile' out result
-
-    buildWaiMiddlewareRollbar </> waiMiddlewareRollbar <> "-" <> waiMiddlewareRollbarVersion %> \out -> do
-      (Exit x, Stdout result) <-
-        cmd (Traced "cabal info") "cabal info" [takeFileName out]
-      case x of
-        ExitFailure _ -> do
-          need [buildWaiMiddlewareRollbar </> ".build", out <.> "tar.gz"]
-          cmd_ (Traced "cabal upload") "cabal upload" [out <.> "tar.gz"]
-          writeFile' out ""
-        ExitSuccess -> writeFile' out result
-
-    packageKatipRollbar </> katipRollbar <.> "cabal" %> \out -> do
-      need [replaceFileName out "package.yaml"]
-      cmd_ (Cwd packageKatipRollbar) (Traced "hpack") "hpack"
-
-    packageRollbarHS </> rollbarHS <.> "cabal" %> \out -> do
-      need [replaceFileName out "package.yaml"]
-      cmd_ (Cwd packageRollbarHS) (Traced "hpack") "hpack"
+build :: Package ->  FilePath
+build = \case
+  Haskell {name} -> buildDir </> packageDir </> name </> ".build"
 
 buildDir :: FilePath
 buildDir = "_build"
-
-buildKatipRollbar :: FilePath
-buildKatipRollbar = buildDir </> packageKatipRollbar
-
-buildRollbarHS :: FilePath
-buildRollbarHS = buildDir </> packageRollbarHS
-
-buildWaiMiddlewareRollbar :: FilePath
-buildWaiMiddlewareRollbar = buildDir </> packageWaiMiddlewareRollbar
 
 ghciFlags :: [String]
 ghciFlags =
@@ -366,32 +98,150 @@ ghciFlags =
   , "-v1"
   ]
 
-katipRollbar :: FilePath
-katipRollbar = "katip-rollbar"
+haskell :: Manifest -> String -> Maybe (NonEmpty String) -> String -> Rules ()
+haskell manifest name tests' version = do
+  root <- liftIO getCurrentDirectory
+  let build' = buildDir </> package
+      package = packageDir </> name
 
-katipRollbarVersion :: FilePath
-katipRollbarVersion = "0.3.0.1"
+  phony ("watch-" <> name) $ do
+    need [build' </> ".configure"]
+    runAfter
+      ( runProcess_
+      $ setWorkingDir package
+      $ proc
+        "ghcid"
+        [ "--command"
+        , "cabal repl lib:"
+          <> name
+          <> " --builddir "
+          <> root </> build'
+          <> " --ghc-options '"
+          <> unwords ghciFlags
+          <> "'"
+        ]
+      )
+
+  build' </> ".build" %> \out -> do
+    srcs <- getDirectoryFiles "" [package </> "src//*.hs"]
+    need ((build' </> ".configure") : srcs)
+    cmd
+      (Cwd package)
+      (FileStdout out)
+      (Traced "cabal build")
+      "cabal build"
+      "--builddir"
+      [root </> build']
+
+  build' </> ".check" %> \out -> do
+    need [package </> name <.> "cabal"]
+    cmd
+      (Cwd package)
+      (EchoStdout True)
+      (FileStdout out)
+      (Traced "cabal check")
+      "cabal check"
+
+  build' </> ".configure" %> \out -> do
+    need [buildDir </> ".update", package </> name <.> "cabal"]
+    cmd
+      (Cwd package)
+      (FileStdout out)
+      (Traced "cabal configure")
+      "cabal configure"
+      "--builddir"
+      [root </> build']
+      ("--enable-tests" <$ tests')
+
+  for_ tests' $ \tests ->
+    for_ tests $ \suite -> do
+      build' </> "build" </> suite </> suite %> \_ -> do
+        srcs <-
+          getDirectoryFiles
+            ""
+            [package </> "src//*.hs", package </> "test//*.hs"]
+        need ((build' </> ".build") : srcs)
+        cmd_
+          (Cwd package)
+          (Traced "cabal build")
+          "cabal build"
+          ["test:" <> suite]
+          "--builddir"
+          [root </> build']
+
+      build' </> "build" </> suite </> suite <.> "out" %> \out -> do
+        need [dropExtension out]
+        cmd_
+          (Cwd package)
+          (FileStdout out)
+          (Traced $ name <> " " <> suite)
+          [((root </>) . dropExtension) out]
+
+  build' </> name <> "-" <> version %> \out -> do
+    (Exit x, Stdout result) <-
+      cmd (Traced "cabal info") "cabal info" [takeFileName out]
+    case x of
+      ExitFailure _ -> do
+        need [build' </> ".build", out <.> "tar.gz"]
+        cmd_ (Traced "cabal upload") "cabal upload" [out <.> "tar.gz"]
+        writeFile' out ""
+      ExitSuccess -> writeFile' out result
+
+  build' </> name <> "-" <> version <.> "tar.gz" %> \_ -> do
+    srcs <- getDirectoryFiles "" [package </> "src//*.hs"]
+    need ((build' </> ".check") : (build' </> ".configure") : srcs)
+    cmd_
+      (Cwd package)
+      (Traced "cabal sdist")
+      "cabal sdist"
+      "--builddir"
+      [root </> build']
+
+  case manifest of
+    Cabal -> mempty
+    Hpack ->
+      package </> name <.> "cabal" %> \out -> do
+        need [replaceFileName out "package.yaml"]
+        cmd_ (Cwd package) (Traced "hpack") "hpack"
 
 packageDir :: FilePath
 packageDir = "packages"
 
-packageKatipRollbar :: FilePath
-packageKatipRollbar = packageDir </> katipRollbar
+packages :: [Package]
+packages =
+  [ Haskell
+    { manifest = Hpack
+    , name = "katip-rollbar"
+    , tests = Nothing
+    , version = "0.3.0.1"
+    }
+  , Haskell
+    { manifest = Hpack
+    , name = "rollbar-hs"
+    , tests = Just ("doc-test" :| [])
+    , version = "0.3.1.0"
+    }
+  , Haskell
+    { manifest = Cabal
+    , name = "wai-middleware-rollbar"
+    , tests = Nothing
+    , version = "0.11.0"
+    }
+  ]
 
-packageRollbarHS :: FilePath
-packageRollbarHS = packageDir </> rollbarHS
+sdist :: Package -> FilePath
+sdist = \case
+  Haskell {name, version} ->
+    buildDir </> packageDir </> name </> name <> "-" <> version <.> "tar.gz"
 
-packageWaiMiddlewareRollbar :: FilePath
-packageWaiMiddlewareRollbar = packageDir </> waiMiddlewareRollbar
+test :: Package -> [FilePath]
+test = \case
+  Haskell {name, tests} -> foldMap (toList . fmap go) tests
+    where
+    go suite =
+      buildDir </> packageDir </> name </> "build" </> suite </> suite <.> "out"
 
-rollbarHS :: FilePath
-rollbarHS = "rollbar-hs"
-
-rollbarHSVersion :: String
-rollbarHSVersion = "0.3.1.0"
-
-waiMiddlewareRollbar :: FilePath
-waiMiddlewareRollbar = "wai-middleware-rollbar"
-
-waiMiddlewareRollbarVersion :: String
-waiMiddlewareRollbarVersion = "0.11.0"
+uploadToHackage :: Package -> FilePath
+uploadToHackage = \case
+  Haskell {name, version} ->
+    buildDir </> packageDir </> name </> name <> "-" <> version
