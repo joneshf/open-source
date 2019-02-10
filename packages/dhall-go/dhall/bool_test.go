6ba2e43ca91d5ef3089d264722a89defa783e583
joneshf <jones3.hardy@gmail.com>
2019-02-09 22:17:08 -0800

    Add a suite for `Bool`
    Fiddling around with pointers and references is a bit annoying.
    Maybe we've setup our types incorrectly?
    Might have to think of a different way to structure things.

--- packages/dhall-go/dhall/bool_test.go
+++ packages/dhall-go/dhall/bool_test.go
@@ -56,3 +56,95 @@ func TestBoolType(t *testing.T) {
 		}
 	})
 }
+
+func TestBool(t *testing.T) {
+	t.Run("alphaNormalize", func(t *testing.T) {
+		actual := (&Bool{Value: false}).alphaNormalize()
+		expected := &Bool{Value: false}
+		if *expected != *actual.(*Bool) {
+			t.Errorf("Expected: %#v, Actual: %#v", expected, actual)
+		}
+
+		actual = (&Bool{Value: true}).alphaNormalize()
+		expected = &Bool{Value: true}
+		if *expected != *actual.(*Bool) {
+			t.Errorf("Expected: %#v, Actual: %#v", expected, actual)
+		}
+	})
+
+	t.Run("betaNormalize", func(t *testing.T) {
+		actual := (&Bool{Value: false}).betaNormalize()
+		expected := &Bool{Value: false}
+		if *expected != *actual.(*Bool) {
+			t.Errorf("Expected: %#v, Actual: %#v", expected, actual)
+		}
+
+		actual = (&Bool{Value: true}).betaNormalize()
+		expected = &Bool{Value: true}
+		if *expected != *actual.(*Bool) {
+			t.Errorf("Expected: %#v, Actual: %#v", expected, actual)
+		}
+	})
+
+	t.Run("encode", func(t *testing.T) {
+		actual := (&Bool{Value: false}).encode()
+		expected := cbor{value: false}
+		if expected != actual {
+			t.Errorf("Expected: %#v, Actual: %#v", expected, actual)
+		}
+
+		actual = (&Bool{Value: true}).encode()
+		expected = cbor{value: true}
+		if expected != actual {
+			t.Errorf("Expected: %#v, Actual: %#v", expected, actual)
+		}
+	})
+
+	t.Run("infer", func(t *testing.T) {
+		actual, err := (&Bool{Value: false}).infer(Context{value: []annotatedExpression{}})
+		expected := &BoolType{}
+		if err != nil {
+			t.Error(err)
+		}
+		if expected != actual.(*BoolType) {
+			t.Errorf("Expected: %#v, Actual: %#v", expected, actual)
+		}
+
+		actual, err = (&Bool{Value: true}).infer(Context{value: []annotatedExpression{}})
+		expected = &BoolType{}
+		if err != nil {
+			t.Error(err)
+		}
+		if expected != actual.(*BoolType) {
+			t.Errorf("Expected: %#v, Actual: %#v", expected, actual)
+		}
+	})
+
+	t.Run("shift", func(t *testing.T) {
+		actual := (&Bool{Value: false}).shift(0, "", 0)
+		expected := &Bool{Value: false}
+		if *expected != *actual.(*Bool) {
+			t.Errorf("Expected: %#v, Actual: %#v", expected, actual)
+		}
+
+		actual = (&Bool{Value: true}).shift(0, "", 0)
+		expected = &Bool{Value: true}
+		if *expected != *actual.(*Bool) {
+			t.Errorf("Expected: %#v, Actual: %#v", expected, actual)
+		}
+	})
+
+	t.Run("substitute", func(t *testing.T) {
+		actual := (&Bool{Value: false}).substitute("", 0, &Bool{Value: true})
+		expected := &Bool{Value: false}
+		if *expected != *actual.(*Bool) {
+			t.Errorf("Expected: %#v, Actual: %#v", expected, actual)
+		}
+
+		actual = (&Bool{Value: true}).substitute("", 0, &Bool{Value: true})
+		expected = &Bool{Value: true}
+		if *expected != *actual.(*Bool) {
+			t.Errorf("Expected: %#v, Actual: %#v", expected, actual)
+		}
+	})
+}
