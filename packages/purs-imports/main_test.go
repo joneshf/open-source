7a056208841d70f4c12fa63098db024d1732b0cb
joneshf <jones3.hardy@gmail.com>
2019-06-29 09:23:19 -0700

    Parse a module in a `string`
    Another thing we'll need is to parse the module name.
    We cannot rely on the file name to tell us what module it is,
    as PureScript doesn't work that way.
    
    Instead, we have to parse the module to figure it out.
    It would be nice if this information was available outside of the
    source language, but that's probably never going to happen for PS.

--- packages/purs-imports/main_test.go
+++ packages/purs-imports/main_test.go
@@ -63,3 +63,47 @@ func TestParseImportFails(t *testing.T) {
 		})
 	}
 }
+
+var parseModuleDoesNotFail = []struct {
+	input    string
+	expected string
+}{
+	{input: "module Data.Array where", expected: "Data.Array"},
+	{input: "module Effect where", expected: "Effect"},
+	{input: "module Prelude where", expected: "Prelude"},
+	{input: "    module           Prelude           where", expected: "Prelude"},
+}
+
+func TestParseModuleDoesNotFail(t *testing.T) {
+	for _, test := range parseModuleDoesNotFail {
+		t.Run(test.input, func(t *testing.T) {
+			actual, ok := parseModule(test.input)
+			if !ok {
+				t.Errorf("Expected %#v to parse: %#v.", test.input, actual)
+			}
+			if test.expected != actual {
+				t.Errorf("Expected: %#v. Actual: %#v.", test.expected, actual)
+			}
+		})
+	}
+}
+
+var parseModuleFails = []struct {
+	input string
+}{
+	{input: ""},
+	{input: "module"},
+	{input: "module' Foo = 12"},
+	{input: "import Foo"},
+}
+
+func TestParseModuleFails(t *testing.T) {
+	for _, test := range parseModuleFails {
+		t.Run(test.input, func(t *testing.T) {
+			actual, ok := parseModule(test.input)
+			if ok {
+				t.Errorf("Expected an error: %s.", actual)
+			}
+		})
+	}
+}
