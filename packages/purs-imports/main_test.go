37e3411f43db860817f3a690c52337c62222c2ca
joneshf <jones3.hardy@gmail.com>
2019-06-29 08:40:38 -0700

    [REBASE] tcr

--- packages/purs-imports/main_test.go
+++ packages/purs-imports/main_test.go
@@ -28,6 +28,18 @@ func TestFindImportDoesNotFailForPrelude(t *testing.T) {
 	}
 }
 
+func TestFindImportDoesNotFailForPreludeWithWhitespace(t *testing.T) {
+	input := "import Prelude"
+	expected := "Prelude"
+	actual, err := findImport(input)
+	if err != nil {
+		t.Error("Did not expect an error", err)
+	}
+	if expected != actual {
+		t.Errorf("Expected: %#v. Actual: %#v.", expected, actual)
+	}
+}
+
 func TestFindImportFailsForTheEmptyString(t *testing.T) {
 	input := ""
 	actual, err := findImport(input)
