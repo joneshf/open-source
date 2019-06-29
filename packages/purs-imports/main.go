a0b45141eef157bf7318b46b769fa165fe50be5c
joneshf <jones3.hardy@gmail.com>
2019-06-29 08:29:30 -0700

    [REBASE] tcr

--- packages/purs-imports/main.go
+++ packages/purs-imports/main.go
@@ -6,7 +6,7 @@ import (
 
 func findImport(str string) (string, error) {
 	tokens := strings.Split(str, " ")
-	if len(tokens) > 2 {
+	if len(tokens) > 1 {
 		importToken := tokens[0]
 		if importToken == "import" {
 			return tokens[1], nil
