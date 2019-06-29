4ddd14b7c8674c64ce12e51e49d178a0301febb4
joneshf <jones3.hardy@gmail.com>
2019-06-29 08:19:15 -0700

    [REBASE] tcr

--- packages/purs-imports/main_test.go
+++ packages/purs-imports/main_test.go
@@ -7,7 +7,7 @@ import (
 func TestFindImportFailsForNonImports(t *testing.T) {
 	expected := ""
 	actual, err := findImport(expected)
-	if false {
+	if err != nil {
 		t.Error("Did not expect an error", err)
 	}
 	if expected != actual {
