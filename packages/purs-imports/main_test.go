2d2ac25547dc27f017aba8e0085fa0419bd4c717
joneshf <jones3.hardy@gmail.com>
2019-06-29 11:18:31 -0700

    [REBASE] tcr

--- packages/purs-imports/main_test.go
+++ packages/purs-imports/main_test.go
@@ -129,16 +129,3 @@ func TestParsePSModuleDoesNotFail(t *testing.T) {
 		})
 	}
 }
-
-func TestParsePSModuleDoesNotFailWithAModuleWithImports(t *testing.T) {
-	input := "module X where\nimport Y\nimport Z"
-	expected := psModule{module: "X", imports: []string{"Y", "Z"}}
-	scanner := bufio.NewScanner(strings.NewReader(input))
-	actual, err := parsePSModule(scanner)
-	if err != nil {
-		t.Errorf("Expected an error: %#v.", actual)
-	}
-	if !reflect.DeepEqual(expected, actual) {
-		t.Errorf("Expected: %#v. Actual: %#v.", expected, actual)
-	}
-}
