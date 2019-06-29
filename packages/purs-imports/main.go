40bf5d431cd67ee7a628f342d5df2dec4f3e7594
joneshf <jones3.hardy@gmail.com>
2019-06-29 09:29:46 -0700

    [REBASE] tcr

--- packages/purs-imports/main.go
+++ packages/purs-imports/main.go
@@ -6,14 +6,7 @@ import (
 )
 
 func findImport(str string) (string, error) {
-	tokens := strings.Fields(str)
-	if len(tokens) > 1 {
-		importToken := tokens[0]
-		if importToken == "import" {
-			return tokens[1], nil
-		}
-	}
-	return str, fmt.Errorf("%#v is not a valid import", str)
+	return findByPrefix("import")(str)
 }
 
 func findModule(str string) (string, error) {
