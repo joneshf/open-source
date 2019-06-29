3889e839357bcc2dab8c150372c484a70469298b
joneshf <jones3.hardy@gmail.com>
2019-06-29 08:23:51 -0700

    [REBASE] tcr

--- packages/purs-imports/main_test.go
+++ packages/purs-imports/main_test.go
@@ -4,17 +4,6 @@ import (
 	"testing"
 )
 
-func TestFindImportAlwaysWorks(t *testing.T) {
-	expected := ""
-	actual, err := findImport(expected)
-	if err != nil {
-		t.Error("Did not expect an error", err)
-	}
-	if expected != actual {
-		t.Errorf("Expected: %#v, Actual: %#v", expected, actual)
-	}
-}
-
 func TestFindImportDoesNotFailForValidInput(t *testing.T) {
 	input := "import Prelude"
 	_, err := findImport(input)
