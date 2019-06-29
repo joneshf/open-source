ec540d8efc49601405b817e25213f363fc914225
joneshf <jones3.hardy@gmail.com>
2019-06-29 10:14:59 -0700

    [REBASE] tcr

--- packages/purs-imports/main.go
+++ packages/purs-imports/main.go
@@ -23,3 +23,21 @@ func findByPrefix(prefix, str string) (string, error) {
 	}
 	return str, fmt.Errorf("%#v is not a valid %s", str, prefix)
 }
+func parseImport(str string) (string, error) {
+	return parseByPrefix("import", str)
+}
+
+func parseModule(str string) (string, error) {
+	return parseByPrefix("module", str)
+}
+
+func parseByPrefix(prefix, str string) (string, error) {
+	tokens := strings.Fields(str)
+	if len(tokens) > 1 {
+		prefixToken := tokens[0]
+		if prefixToken == prefix {
+			return tokens[1], nil
+		}
+	}
+	return str, fmt.Errorf("%#v is not a valid %s", str, prefix)
+}
