5324654c6df9ae97b6e5d517efbac62ac7635eb6
joneshf <jones3.hardy@gmail.com>
2019-06-29 12:28:43 -0700

    [REBASE] tcr

--- packages/purs-imports/main.go
+++ packages/purs-imports/main.go
@@ -21,6 +21,7 @@ func parseModule(str string) (string, bool) {
 }
 
 func parsePSModule(scanner *bufio.Scanner) (psModule, error) {
+	parsedModuleOk := false
 	result := psModule{}
 	for scanner.Scan() {
 		parsedModule, parsedModuleOk := parseModule(scanner.Text())
@@ -35,6 +36,9 @@ func parsePSModule(scanner *bufio.Scanner) (psModule, error) {
 			sort.Strings(result.imports)
 			return result, nil
 		}
+	}
+	if parsedModuleOk {
+		return result, nil
 	}
 	return psModule{}, fmt.Errorf("Could not parse module")
 }
