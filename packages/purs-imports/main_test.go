c11cfbbfd49e692b5cd1cb450f052c4e03d3b7a2
joneshf <jones3.hardy@gmail.com>
2019-06-29 11:44:51 -0700

    [REBASE] tcr

--- packages/purs-imports/main_test.go
+++ packages/purs-imports/main_test.go
@@ -7,6 +7,50 @@ import (
 	"testing"
 )
 
+var parseImportDoesNotFail = []struct {
+	input    string
+	expected string
+}{
+	{input: "import Data.Array", expected: "Data.Array"},
+	{input: "import Effect", expected: "Effect"},
+	{input: "import Prelude", expected: "Prelude"},
+	{input: "    import           Prelude          ", expected: "Prelude"},
+}
+
+func TestParseImportDoesNotFail(t *testing.T) {
+	for _, test := range parseImportDoesNotFail {
+		t.Run(test.input, func(t *testing.T) {
+			actual, ok := parseImport(test.input)
+			if !ok {
+				t.Errorf("Expected %#v to parse: %#v.", test.input, actual)
+			}
+			if test.expected != actual {
+				t.Errorf("Expected: %#v. Actual: %#v.", test.expected, actual)
+			}
+		})
+	}
+}
+
+var parseImportFails = []struct {
+	input string
+}{
+	{input: ""},
+	{input: "import"},
+	{input: "import' Foo = 12"},
+	{input: "module Foo"},
+}
+
+func TestParseImportFails(t *testing.T) {
+	for _, test := range parseImportFails {
+		t.Run(test.input, func(t *testing.T) {
+			actual, ok := parseImport(test.input)
+			if ok {
+				t.Errorf("Expected %#v not to parse: %#v.", test.input, actual)
+			}
+		})
+	}
+}
+
 var oldParseImportDoesNotFail = []struct {
 	input    string
 	expected string
