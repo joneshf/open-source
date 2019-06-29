3c60af91175f245131168442927bad12589e4bc6
joneshf <jones3.hardy@gmail.com>
2019-06-29 11:12:10 -0700

    [REBASE] tcr

--- packages/purs-imports/main_test.go
+++ packages/purs-imports/main_test.go
@@ -104,6 +104,19 @@ func TestParsePSModuleFailsForTheEmptyString(t *testing.T) {
 	}
 }
 
+func TestParsePSModuleDoesNotFail(t *testing.T) {
+	input := "module X where"
+	expected := psModule{module: "X"}
+	scanner := bufio.NewScanner(strings.NewReader(input))
+	actual, err := parsePSModule(scanner)
+	if err != nil {
+		t.Errorf("Expected an error: %#v.", actual)
+	}
+	if !reflect.DeepEqual(expected, actual) {
+		t.Errorf("Expected: %#v. Actual: %#v.", expected, actual)
+	}
+}
+
 func TestParsePSModuleDoesNotFailWithATrivialModule(t *testing.T) {
 	input := "module X where"
 	expected := psModule{module: "X"}
