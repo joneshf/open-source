db4e5c0ffda648ff3d86a2256cd60c6c66e9291e
joneshf <jones3.hardy@gmail.com>
2019-06-29 08:45:03 -0700

    [REBASE] tcr

--- packages/purs-imports/main_test.go
+++ packages/purs-imports/main_test.go
@@ -5,8 +5,8 @@ import (
 )
 
 func TestFindImportDoesNotFail(t *testing.T) {
+	input := "import Data.Array"
 	t.Run("import Data.Array", func(t *testing.T) {
-		input := "import Data.Array"
 		expected := "Data.Array"
 		actual, err := findImport(input)
 		if err != nil {
