14ff0a66a9d792e3cd7c14ef642bcc7fd32a90f6
joneshf <jones3.hardy@gmail.com>
2019-06-30 06:16:51 -0700

    [REBASE] tcr

--- packages/purs-imports/main.go
+++ packages/purs-imports/main.go
@@ -41,7 +41,7 @@ func findModule(scanner *bufio.Scanner) (module string, err error) {
 func graph(module psModule) string {
 	var builder strings.Builder
 	builder.WriteString("digraph imports {")
-	fmt.Fprintf(&builder, `  %#v`, module.module)
+	fmt.Fprintf(&builder, "  %#v", module.module)
 	builder.WriteString("}")
 	return builder.String()
 }
