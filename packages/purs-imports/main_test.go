d9480ec8d0cdaf5faf3ae093092e509fed507db3
joneshf <jones3.hardy@gmail.com>
2019-07-01 04:35:46 -0700

    [REBASE] tcr

--- packages/purs-imports/main_test.go
+++ packages/purs-imports/main_test.go
@@ -19,6 +19,19 @@ func TestGraph(t *testing.T) {
 	}
 }
 
+func TestGraphNew(t *testing.T) {
+	input := psModule{module: "Main", imports: []string{"Effect", "Prelude"}}
+	expected := `digraph imports {
+  "Main";
+  "Main" -> "Effect";
+  "Main" -> "Prelude";
+}`
+	actual := graph(input)
+	if !reflect.DeepEqual(expected, actual) {
+		t.Errorf("Expected: %#v. Actual: %#v.", expected, actual)
+	}
+}
+
 var parseImportDoesNotFail = []struct {
 	input    string
 	expected string
